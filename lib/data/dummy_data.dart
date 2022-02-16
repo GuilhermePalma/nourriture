1bfa6446e7774f2ef2658a2c12ab2780a57b84b9
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-02-16 11:35:18 -0300

    Commit Inicial

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:nourriture/models/category.dart';
+
+const dummyCategory = [
+  Category(
+    id: 'c1',
+    name: 'Italiano',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    name: 'Rápido & Fácil',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    name: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    name: 'Alemã',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    name: 'Leve & Saudável',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c6',
+    name: 'Exótica',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    name: 'Café da Manhã',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    name: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    name: 'Francesa',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    name: 'Verão',
+    color: Colors.teal,
+  ),
+];
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:nourriture/components/category_item.dart';
+import 'package:nourriture/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Vamos Cozinhar ?"),
+      ),
+      body: GridView(
+        // Sliver= Area que permite Scrool e GridDelegate= Como a Grid será renderizada
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategory.map((categoryElement) {
+          return CategoryItem(category: categoryElement);
+        }).toList(),
+      ),
+    );
+  }
+}
