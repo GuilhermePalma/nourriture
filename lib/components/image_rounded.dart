b3795f9ebef8b7975f2b62efe444f3b3ebf3fb92
Guilherme <guilherme.palma8@etec.sp.gov.br>
2022-02-24 23:28:38 -0300

    Add Animation Loading in Images

--- lib/components/image_rounded.dart
+++ lib/components/image_rounded.dart
@@ -23,6 +23,7 @@ class ImageRounded extends StatelessWidget {
         bottomLeft: Radius.circular(radiusBottom),
         bottomRight: Radius.circular(radiusBottom),
       ),
+      // Verifica qual o Tipo de Imagem (Asset ou URL)
       child: urlImage != null
           ? Image.network(
               urlImage!,
@@ -31,14 +32,45 @@ class ImageRounded extends StatelessWidget {
                 // Tratamento de Erro na Imagem
                 return Image.asset("assets/images/error_404.jpg");
               },
+              // Adiciona o CircularProgress enquanto carrega a Imagem Online
+              loadingBuilder: (BuildContext context, Widget child,
+                  ImageChunkEvent? loadingProgress) {
+                if (loadingProgress != null) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                } else {
+                  return child;
+                }
+              },
             )
           : assetImage != null
-              ? Image.asset(
-                  assetImage!,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    // Tratamento de Erro na Imagem
-                    return Image.asset("assets/images/error_404.jpg");
+              ? FutureBuilder(
+                  // Cria um Evento Futuro que leva 3 segundos para carregar a Image Asset
+                  future: Future.delayed(const Duration(seconds: 3)),
+                  builder: (cntx, asyncSnapshot) {
+                    return asyncSnapshot.connectionState == ConnectionState.done
+                        ? Image.asset(
+                            assetImage!,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) {
+                              // Tratamento de Erro na Imagem
+                              return Image.asset("assets/images/error_404.jpg");
+                            },
+                          )
+                        : const Center(
+                            // Contrução do Loading da Imagem
+                            child: SizedBox(
+                              height: 20,
+                              width: 20,
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
                   },
                 )
               : Image.asset(
